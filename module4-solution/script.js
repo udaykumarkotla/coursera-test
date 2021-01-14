818980b1bd2b122ef51346647be4413000e344a3
udaykumarkotla <kudaykumar.18.cse@anits.edu.in>
2021-01-14 09:06:02 +0530

    module-4 assign

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,6 +1,6 @@
 
 (function() {
-    var names = ["uday", "supriya", "Jyoti", "Joshna", "Priya", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
